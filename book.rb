ba92523fc930e4ade82f548fcc6be56ee70e3527
RoyJumah <royjumah17@gmail.com>
2023-03-09 11:57:48 +0300

    create association between book and rental

--- book.rb
+++ book.rb
@@ -1,10 +1,19 @@
+require_relative 'rental'
+
 class Book
   attr_accessor :title, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
 
   end
-  
+
+def add_rentals(rental)
+
+  @rentals.push(rental)
+end
+
 end
