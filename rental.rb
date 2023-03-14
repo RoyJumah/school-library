b1b3f8b7119b544ca17634e6068928739829a551
RoyJumah <royjumah17@gmail.com>
2023-03-14 17:33:32 +0300

    add rentals.json file

--- rental.rb
+++ rental.rb
@@ -8,4 +8,25 @@ class Rental
     person.rentals << self
     book.rentals << self
   end
+
+  def to_hash
+    {
+      date: @date,
+      book_title: @book.title,
+      person_id: @person.id
+    }
+  end
+
+  def self.from_hash(hash)
+    date = hash[:date]
+    book_title = hash[:book_title]
+    person_id = hash[:person_id]
+
+    # Look up the book and person objects based on the title and id
+    book = Book.all.find { |b| b.title == book_title }
+    person = Person.all.find { |p| p.id == person_id }
+
+    # Create a new rental object
+    Rental.new(date, person, book)
+  end
 end
