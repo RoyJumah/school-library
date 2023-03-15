6e206380ce53414c8e41183c931d25b4f2ea20b4
Donald Akite <donaldakite27@gmail.com>
2023-03-15 19:24:52 +0000

    Fix offenses

--- rental.rb
+++ rental.rb
@@ -2,6 +2,7 @@ require_relative 'person'
 require_relative 'book'
 class Rental
   attr_accessor :date, :book, :person
+
   def initialize(date, book, person)
     @date = date
     @book = book
@@ -9,12 +10,15 @@ class Rental
     @person = person
     person.rentals << self
   end
+
   def self.all
     ObjectSpace.each_object(self).to_a
   end
+
   def to_s
     "Date: #{date}, Book \"#{book.title}\" by #{book.author}, Rented to: #{person.name}"
   end
+
   def self.create_rental
     puts
     book_class = Book.select_book_for_renting
@@ -29,12 +33,14 @@ class Rental
     puts rental
     rental.save
   end
+
   def self.rentals_by_person_id
     puts 'ID of person:'
     id = gets.chomp.to_i
     rentals = Rental.load_rentals
     rentals.find { |rental| rental['id'].to_i == id }
   end
+
   def self.list_rentals
     persons_rentals = rentals_by_person_id
     puts
@@ -42,6 +48,7 @@ class Rental
     puts "date: #{persons_rentals['date']} book: #{persons_rentals['book']} rented by: #{persons_rentals['name']}"
     persons_rentals
   end
+
   def save
     if File.exist?('rentals.json')
       rentals_file = File.read('rentals.json')
@@ -53,8 +60,10 @@ class Rental
                                                          name: person.name }]))
     end
   end
+
   def self.load_rentals
     return unless File.exist?('rentals.json')
+
     rentals_file = File.read('rentals.json')
     JSON.parse(rentals_file)
   end
