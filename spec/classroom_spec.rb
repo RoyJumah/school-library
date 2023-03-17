f5f58164e368c3cb914fb2c58a1502e8ed384143
RoyJumah <royjumah17@gmail.com>
2023-03-17 07:27:44 +0300

    fix classroom unit test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,11 +1,9 @@
 require_relative 'spec_helper'
 
 describe Classroom do
-  Classroom.new(101)
-  Classroom.new(102)
-
   it 'should create a classroom' do
-    expect(Classroom.all[0].label).to eql(102)
-    expect(Classroom.all[1].label).to eql(101)
+    Classroom.new(101)
+    Classroom.new(102)
+    expect(Classroom.all.map(&:label)).to include(101, 102)
   end
 end
