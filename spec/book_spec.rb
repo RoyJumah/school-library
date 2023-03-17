5adb0aca91f0cb9b7fda22e28dd30ecbb1ffdf86
RoyJumah <royjumah17@gmail.com>
2023-03-17 07:16:38 +0300

    fixing error in tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe Book do
   before(:each) do
-    @book = Book.new('Rich Dad Poor Dad', 'Robert Kiyosaki')
+    @book = Book.new('The Hobbit', 'J.R.R. Tolkien')
   end
 
   it 'class initializes' do
@@ -10,7 +10,7 @@ describe Book do
   end
 
   it 'attributes are accessible' do
-    expect(@book.title).to eq('Rich Dad Poor Dad')
-    expect(@book.author).to eq('Robert Kiyosaki')
+    expect(@book.title).to eq('The Hobbit')
+    expect(@book.author).to eq('J.R.R. Tolkien')
   end
 end
